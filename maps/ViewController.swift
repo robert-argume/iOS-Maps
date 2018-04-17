075883a0781a8094a6c5ee445cafaf3b0b865948
Robert <robert.a.argume@gmail.com>
2018-04-16 20:28:02 -0400

    Add button code to open location in Map App - closes #6

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -40,6 +40,16 @@ class ViewController: UIViewController {
     }
 
 
-
+    @IBAction func seeInMapApp(_ sender: UIButton) {
+        let placemark = MKPlacemark(coordinate: self.mapLocation,  addressDictionary: nil)
+        let mapitem = MKMapItem(placemark: placemark)
+        mapitem.name = "A really cool place"
+        mapitem.openInMaps(launchOptions:[
+            MKLaunchOptionsMapTypeKey: MKMapType.standard.rawValue,
+            MKLaunchOptionsMapCenterKey: self.map.region.center,
+            MKLaunchOptionsMapSpanKey: self.map.region.span
+            ])
+    }
+    
 }
 

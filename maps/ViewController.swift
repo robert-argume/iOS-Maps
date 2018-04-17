6e4da6f00e9696cae37b25487793984cbc834f8e
Robert <robert.a.argume@gmail.com>
2018-04-16 20:06:15 -0400

    Add Toronto as default location - closes # 5

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-// Toronto 43.6532° N, 79.3832° W
+    // Toronto 43.6532° N, 79.3832° W
+    let defaultLongitude = -79.3832
+    let defaultLatitude = 43.6532
+    var delta = 0.5
+    
+    @IBOutlet weak var map: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let mapLocation = CLLocationCoordinate2D(latitude: defaultLatitude, longitude: defaultLongitude)
+//
+        let loc = CLLocationCoordinate2D(latitude: defaultLatitude, longitude: defaultLongitude);
+        
+        let span = MKCoordinateSpanMake(delta, delta);
+        
+        let reg = MKCoordinateRegionMake(loc, span)
+        
+        self.map.region = reg
+        
     }
 
 

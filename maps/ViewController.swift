8e9f4aa0414abb5ab9114a679fc423cd4fe01258
Robert <robert.a.argume@gmail.com>
2018-04-16 20:16:43 -0400

    Add default annotation and marker - closes #9

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -14,14 +14,15 @@ class ViewController: UIViewController {
     let defaultLongitude = -79.3832
     let defaultLatitude = 43.6532
     var delta = 0.5
+    let mapLocation = CLLocationCoordinate2D(latitude: 43.6532, longitude: -79.3832)
+    
     
     @IBOutlet weak var map: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let mapLocation = CLLocationCoordinate2D(latitude: defaultLatitude, longitude: defaultLongitude)
-//
+
         let loc = CLLocationCoordinate2D(latitude: defaultLatitude, longitude: defaultLongitude);
         
         let span = MKCoordinateSpanMake(delta, delta);
@@ -29,6 +30,12 @@ class ViewController: UIViewController {
         let reg = MKCoordinateRegionMake(loc, span)
         
         self.map.region = reg
+        
+        let ann = MKPointAnnotation()
+        ann.coordinate = self.mapLocation
+        ann.title = "Toronto"
+        ann.subtitle = "Fun in Toronto"
+        self.map.addAnnotation(ann)
         
     }
 
